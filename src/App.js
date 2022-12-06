ce8c4d93f9f561d8c1604b1927eb82d69a43cd32
touhidcodes <touhidcodes@gmail.com>
2022-12-06 23:51:11 +0600

    react core

--- src/App.js
+++ src/App.js
@@ -63,7 +63,11 @@ function Count() {
       <h3>Count:{count}</h3>
       <button onClick={() => setCount(count + 1)}>Increase</button>
       <button onClick={() => setCount(count - 1)}>Decrease</button>
+      <h3>Count:{count}</h3>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+      <button onClick={() => setCount(count - 1)}>Decrease</button>
     </div>
   );
 }
+
 export default App;
